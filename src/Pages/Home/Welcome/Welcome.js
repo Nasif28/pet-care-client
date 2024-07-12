d4909fe330cde6870680f44ccf6351ae3ce04a71
Nasif Jihan <nasifzeehan1@gmail.com>
2024-07-12 16:31:38 +0600

    modify

--- src/Pages/Home/Welcome/Welcome.js
+++ src/Pages/Home/Welcome/Welcome.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import './Welcome.css';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
 
 const Welcome = () => {
     return (
-        <div id="welcome" className="welcome">
-            <Row className="container-fluid p-5">
-                <Col className="col-lg-6 col-md-12">
-                    <img src={'https://amplifythemes.com/petenica/wp-content/uploads/2018/03/image-3.jpg'} alt="" />
-                </Col>
-                <Col className="col-lg-6 col-md-12 text-start pt-5 ps-5 text-white">
-                    <h1 className="fw-bold text-success">Welcome to Pet Care</h1>
-                    <p>Broadcast neglectful and poignantly well until and some listlessly amidst suc cessful concentrically ably dachshund more far but forwardly echidna outside tiger split thanks far vibrantly gosh hence pangolin however notwithstanding leapt untruthful gauchely yikes komodo dully more.</p>
-                    <p>As abandoned winced this more far wow jeepers near more wow goodness so revealed much along worm some grasshopper.</p>
-                    <br />
-                    <small className="text-danger"><i> - Nasif Zeehan</i></small>
-                </Col>
-            </Row>
+        <div id="welcome" className="welcome-section">
+            <Container className="p-5">
+                <Row className="align-items-center">
+                    <Col lg={6} md={12} className="mb-4 mb-lg-0">
+                        <img
+                            src="https://i.ibb.co/c3pY4W6/Cute-domestic-cat-looking.jpg"
+                            alt="Welcome to Pet Care"
+                            className="img-fluid rounded"
+                        />
+                    </Col>
+                    <Col lg={6} md={12} className="text-start  ps-5">
+                        <h1 className="fw-bold text-success">Welcome to Pet Care</h1>
+                        <p>
+                            Broadcast neglectful and poignantly well until and some listlessly amidst successful concentrically ably dachshund more far but forwardly echidna outside tiger split thanks far vibrantly gosh hence pangolin however notwithstanding leapt untruthful gauchely yikes komodo dully more.
+                        </p>
+                        <p>
+                            As abandoned winced this more far wow jeepers near more wow goodness so revealed much along worm some grasshopper.
+                        </p>
+                        <br />
+                        <small className="text-danger">
+                            <i> - Nasif Zeehan</i>
+                        </small>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -3,35 +3,39 @@ import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 import useAuth from './../../../Hooks/useAuth';
+import './Header.css'; // Import the custom CSS file
 
 const Header = () => {
     const { user, logOut } = useAuth();
     return (
         <>
-            <Navbar bg="light" variant="light" sticky="top" collapseOnSelect expand="lg" >
+            <Navbar bg="light" variant="light" sticky="top" collapseOnSelect expand="lg" className="custom-navbar">
                 <Container>
-                    <Navbar.Brand href="/home" className="fw-bolder text-success ">PET CARE</Navbar.Brand>
+                    <Navbar.Brand href="/home" className="fw-bolder text-success custom-brand">PET CARE</Navbar.Brand>
                     <Navbar.Toggle />
                     <Navbar.Collapse className="justify-content-center">
-                        <Nav.Link as={HashLink} to="/home#home">Home</Nav.Link>
-                        <Nav.Link as={HashLink} to="/home#services">Services</Nav.Link>
-                        <Nav.Link as={HashLink} to="/home#clients">Clients</Nav.Link>
-                        <Nav.Link as={HashLink} to="/home#welcome">Welcome</Nav.Link>
-                        <Nav.Link as={HashLink} to="/vats#vats">Vats</Nav.Link>
-                        <Nav.Link as={HashLink} to="/about#about">About</Nav.Link>
-
-                        {user?.email ?
-                            <Navbar.Text className="ms-auto">
-                                Signed in as: <a href="#login">{user?.displayName}</a>
-                                {user?.photoURL ?
-                                    <img className="mx-3 border border-success border-2 rounded-circle" src={user?.photoURL} width="40" height="40" alt="" /> :
-                                    <img className="mx-3 border border-success border-2 rounded-circle" src={'https://dm0qx8t0i9gc9.cloudfront.net/watermarks/image/rDtN98Qoishumwih/scared-cartoon-face-expression_XkDKCZ_SB_PM.jpg'} width="40" height="40" alt="" />
-                                }
-                                <Button onClick={logOut} className="btn fw-bolder btn-success me-3">Logout</Button>
+                        <Nav className="mx-auto">
+                            <Nav.Link as={HashLink} to="/home#home" className="custom-nav-link">Home</Nav.Link>
+                            <Nav.Link as={HashLink} to="/home#services" className="custom-nav-link">Services</Nav.Link>
+                            <Nav.Link as={HashLink} to="/home#clients" className="custom-nav-link">Clients</Nav.Link>
+                            <Nav.Link as={HashLink} to="/home#welcome" className="custom-nav-link">Welcome</Nav.Link>
+                            <Nav.Link as={HashLink} to="/vats#vats" className="custom-nav-link">Vats</Nav.Link>
+                            <Nav.Link as={HashLink} to="/about#about" className="custom-nav-link">About</Nav.Link>
+                        </Nav>
+                        {user?.email ? (
+                            <Navbar.Text className=" d-flex align-items-center">
+                                <span className="me-2">Signed in as:</span>
+                                <a href="#login" className="fw-bold text-decoration-none">{user?.displayName}</a>
+                                {user?.photoURL ? (
+                                    <img className="mx-3 border border-success border-2 rounded-circle" src={user?.photoURL} width="40" height="40" alt="User" />
+                                ) : (
+                                    <img className="mx-3 border border-success border-2 rounded-circle" src={'https://dm0qx8t0i9gc9.cloudfront.net/watermarks/image/rDtN98Qoishumwih/scared-cartoon-face-expression_XkDKCZ_SB_PM.jpg'} width="40" height="40" alt="User" />
+                                )}
+                                <Button onClick={logOut} className="btn fw-bolder btn-success ms-3">Logout</Button>
                             </Navbar.Text>
-                            :
-                            <Nav.Link as={Link} to="/login">Login</Nav.Link>}
-
+                        ) : (
+                            <Nav.Link as={Link} to="/login" className="custom-nav-link">Login</Nav.Link>
+                        )}
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
